d1922d00e274bea99b49ddd8b00cb46ca895600b
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-11-06 12:37:34 +0100

    Completement dynamique

--- src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package org.sid.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +16,34 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+
+    //Cette methode, pour dire au gateway qu'à chaque fois tu reçois une requette,
+    // regardre dans son url et tu vas voir le nom de son micro-service, c'est complétement dynamique
+    // et il va router la requette vers le bon micro-service
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties){
+
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
+
+
+
+
+
+
+
+
+
+        /*  Methode plus ou moins dynamique
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
-                .route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+                .route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route((r)->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
 
     }
+    */
 }
